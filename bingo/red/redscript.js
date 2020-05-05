2e10ebe283922ae606eaa13a889cb52d632de76e
bpmxjordy <48060710+bpmxjordy@users.noreply.github.com>
2020-05-05 15:54:08 +0100

    Add files via upload

---
+++ bingo/red/redscript.js
@@ -0,0 +1,60 @@
+
+
+function clicked() {
+    var x = event.target.id;
+    console.log(x);
+    document.getElementById(x).style.background = "#FF2525";
+
+    if (localStorage.getItem(x) == "true") {
+        console.log("1");
+        localStorage.setItem(x, "false");
+        console.log(x);
+        document.getElementById(x).style.background = "white";
+        document.getElementById(x).style.color = "black";
+    }
+    else  {
+        console.log("2");
+        localStorage.setItem(x, "true");
+        console.log(x);
+        ;
+        document.getElementById(x).style.background = "#FF2525";
+        document.getElementById(x).style.color = "white";
+    }
+
+    if (x == "cell12") {
+        document.getElementById(x).style.background = "#FF2525";
+        document.getElementById(x).style.color = "white";
+    }
+
+
+}
+
+function reset() {
+    for (i = 0; i < 25; i++) {
+        var y = cell.concat(i);
+        localStorage.setItem(y, "false");
+        document.getElementById(y).style.background = "white";
+        document.getElementById(y).style.color = "black";
+
+        if (i == "12") {
+            document.getElementById(y).style.background = "#FF2525";
+            document.getElementById(y).style.color = "white";
+        }
+        
+    }
+}
+
+function cleared() {
+
+    for (i = 0; i < 25; i++) {
+        var z = cell.concat(i);
+        localStorage.removeItem(z);
+        document.getElementById(z).style.background = "white";
+        document.getElementById(z).style.color = "black";
+
+        if (i == "12") {
+            document.getElementById(z).style.background = "#FF2525";
+            document.getElementById(z).style.color = "white";
+        }
+    }
+}
---
+++ bingo/yellow/yellowscript.js
@@ -0,0 +1,59 @@
+
+
+function clicked() {
+    var x = event.target.id;
+    console.log(x);
+    document.getElementById(x).style.background = "#FAFB7E";
+
+    if (localStorage.getItem(x) == "true") {
+        console.log("1");
+        localStorage.setItem(x, "false");
+        console.log(x);
+        document.getElementById(x).style.background = "white";
+        document.getElementById(x).style.color = "black";
+    }
+    else  {
+        console.log("2");
+        localStorage.setItem(x, "true");
+        console.log(x);
+        ;
+        document.getElementById(x).style.background = "#FAFB7E";
+   
+    }
+
+    if (x == "cell12") {
+        document.getElementById(x).style.background = "#FAFB7E";
+  
+    }
+
+
+}
+
+function reset() {
+    for (i = 0; i < 25; i++) {
+        var y = cell.concat(i);
+        localStorage.setItem(y, "false");
+        document.getElementById(y).style.background = "white";
+        document.getElementById(y).style.color = "black";
+
+        if (i == "12") {
+            document.getElementById(y).style.background = "#FAFB7E";
+
+        }
+        
+    }
+}
+
+function cleared() {
+
+    for (i = 0; i < 25; i++) {
+        var z = cell.concat(i);
+        localStorage.removeItem(z);
+        document.getElementById(z).style.background = "white";
+        document.getElementById(z).style.color = "black";
+
+        if (i == "12") {
+            document.getElementById(z).style.background = "#FAFB7E";
+        }
+    }
+}
